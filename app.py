d16389e59661958f2f6d9238514fe598dfa40ef3
SeanNaren <sean@grid.ai>
2022-05-20 11:21:29 +0100

    Fix check/message

--- app.py
+++ app.py
@@ -27,13 +27,12 @@ class CheckEnvironmentWork(LightningWork):
         self.debug = debug
 
     def run(self):
-        print("running...")
         setup = EnvironmentChecker(debug=self.debug)
         self.linux = setup.check_linux()
         self.cuda, self.devices = setup.check_cuda_available()
         self.internet = setup.sufficient_internet()
-        self.python = setup.check_python_environment()
-        self.memory = setup.check_memory()
+        self.python = setup.suitable_python_environment()
+        self.memory = setup.sufficient_memory()
         self.bandwidth = str(setup.bandwidth()) + "GB/s"
         self.current_memory = (
             "/".join([f"{gpu:.1f}" for gpu in setup.cuda_memory()]) + "GiB"
@@ -113,6 +112,7 @@ class SetupFlow(LightningFlow):
 
     def run(self):
         if self.start:
+            # todo: this keeps getting called. start is always true even if set false.
             self.environment_check.run()
             self.start = False
 
--- lightning_collaborative/components/env_checker.py
+++ lightning_collaborative/components/env_checker.py
@@ -2,6 +2,7 @@ import importlib
 import operator
 import platform
 import subprocess
+from typing import List
 from xml.etree import ElementTree
 
 from packaging.version import Version
@@ -9,10 +10,10 @@ from packaging.version import Version
 
 class EnvironmentChecker:
     def __init__(
-        self,
-        debug: bool = False,
-        minimum_bandwidth_gb: int = 2,
-        min_cuda_memory_gb: int = 8,
+            self,
+            debug: bool = False,
+            minimum_bandwidth_gb: int = 2,
+            min_cuda_memory_gb: int = 8,
     ):
         self.debug = debug
         self.minimum_bandwidth_gb = minimum_bandwidth_gb
@@ -38,20 +39,25 @@ class EnvironmentChecker:
     def sufficient_internet(self):
         return True
 
-    def check_python_environment(self):
+    def suitable_python_environment(self):
         return operator.ge(Version(platform.python_version()), Version("3.0.0"))
 
-    def check_memory(self):
-        return True
+    def sufficient_memory(self):
+        return not any(gpu <= self.min_cuda_memory_gb for gpu in self.cuda_memory())
 
     def bandwidth(self):
         return 1.5
 
-    def cuda_memory(self):
-        ## does not work on Jetsons... (don't have nvidia-smi)
-        res = subprocess.run(["nvidia-smi", "-q", "-x"], capture_output=True)
-        if res.returncode != 0:
-            return False
+    def cuda_memory(self) -> List[float]:
+        if not self.check_cuda_available():
+            return [0.]
+        # does not work on Jetsons... (don't have nvidia-smi)
+        try:
+            res = subprocess.run(["nvidia-smi", "-q", "-x"], capture_output=True)
+            if res.returncode != 0:
+                return [0.]
+        except FileNotFoundError:
+            return [0.]
         nvidia_smi_log = ElementTree.fromstring(res.stdout)
         gpus = []
         for gpu in nvidia_smi_log.findall("gpu"):
@@ -76,19 +82,24 @@ class EnvironmentChecker:
             warning += "Your machine does not support the minimal requirements (Requires Linux and Python 3)."
         if self.successful():
             if self.bandwidth() <= self.minimum_bandwidth_gb:
+                if self.check_cuda_available():
+                    warning += (
+                        "\nTorch with CUDA is not available on this machine. "
+                        "Make sure you install Torch with CUDA: https://pytorch.org/."
+                    )
+                elif not self.sufficient_memory():
+                    warning += (
+                        f"\nThere is less CUDA memory in some cards than recommended. "
+                        f"Recommend minimum CUDA memory: {self.min_cuda_memory_gb}GiB"
+                    )
                 warning += (
-                    "The internet bandwidth is less than recommended. "
+                    "\nThe internet bandwidth is less than recommended. "
                     "You may see a lot of out of sync/timeout "
                     "errors as a result."
-                )
-            if any(gpu <= self.min_cuda_memory_gb for gpu in self.cuda_memory()):
-                warning += (
-                    f"\nThere is less CUDA memory in some cards than recommended. "
-                    f"Recommend minimum CUDA memory: {self.min_cuda_memory_gb}GiB"
                 )
         return warning
 
     def successful(self):
         if self.debug:
             return True
-        return self.check_python_environment() and self.check_linux()
+        return self.suitable_python_environment() and self.check_linux()
