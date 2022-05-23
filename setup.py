c78563188a9b6564a3c4d81322f67679e6106d6a
SeanNaren <sean@grid.ai>
2022-05-23 18:44:41 +0100

    Add missing files

--- setup.py
+++ setup.py
@@ -1,9 +1,44 @@
+#!/usr/bin/env python
+
+import os
+from importlib.util import module_from_spec, spec_from_file_location
+
 from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+
+def _load_py_module(fname, pkg="lightning_collaborative"):
+    spec = spec_from_file_location(
+        os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname)
+    )
+    py = module_from_spec(spec)
+    spec.loader.exec_module(py)
+    return py
+
+
+about = _load_py_module("__about__.py")
+setup_tools = _load_py_module("setup_tools.py")
+long_description = setup_tools._load_readme_description(
+    _PATH_ROOT, homepage=about.__homepage__, ver=about.__version__
+)
 
 setup(
     name="lightning_collaborative",
-    version="0.0.1",
-    author="SeanNaren",
-    packages=find_packages(),
+    version=about.__version__,
+    description=about.__docs__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    url=about.__homepage__,
+    download_url="https://github.com/PyTorchLightning/lightning-collaborative",
+    license=about.__license__,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
     zip_safe=False,
+    keywords=["deep learning", "pytorch", "AI"],
+    python_requires=">=3.7",
+    setup_requires=["wheel"],
+    install_requires=setup_tools._load_requirements(_PATH_ROOT),
 )
