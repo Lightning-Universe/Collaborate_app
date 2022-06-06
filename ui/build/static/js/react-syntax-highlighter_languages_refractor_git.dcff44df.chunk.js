c87098479616ad51c775488d4503729e035ae7fd
SeanNaren <sean@grid.ai>
2022-06-06 10:40:36 +0100

    Remove build

--- ui/build/static/js/react-syntax-highlighter_languages_refractor_git.dcff44df.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcreate_react_app=self.webpackChunkcreate_react_app||[]).push([[2564],{5120:function(e){function t(e){e.languages.git={comment:/^#.*/m,deleted:/^[-\u2013].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}e.exports=t,t.displayName="git",t.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.dcff44df.chunk.js.map
