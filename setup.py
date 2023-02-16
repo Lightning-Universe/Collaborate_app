a9383a184a5433d0797d0a41dcd79f246bb8bac3
Jirka <jirka.borovec@seznam.cz>
2023-02-16 19:29:36 +0100

    general update

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
-
 import os
 from importlib.util import module_from_spec, spec_from_file_location
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
 _PATH_ROOT = os.path.dirname(__file__)
 
 
-def _load_py_module(fname, pkg="collaborate"):
+def _load_py_module(fname: str, pkg: str = "lit_slack"):
     spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_ROOT, pkg, fname))
     py = module_from_spec(spec)
     spec.loader.exec_module(py)
     return py
 
 
+def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
+
+
 about = _load_py_module("__about__.py")
-setup_tools = _load_py_module("setup_tools.py")
-long_description = setup_tools._load_readme_description(_PATH_ROOT, homepage=about.__homepage__, ver=about.__version__)
+with open(os.path.join(_PATH_ROOT, "README.md"), encoding="utf-8") as fo:
+    readme = fo.read()
+
 
 setup(
     name="collaborate",
@@ -29,12 +35,12 @@ setup(
     download_url="https://github.com/PyTorchLightning/collaborate",
     license=about.__license__,
     packages=find_packages(exclude=["tests", "tests.*"]),
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
     keywords=["deep learning", "pytorch", "AI"],
     python_requires=">=3.7",
     setup_requires=["wheel"],
-    install_requires=setup_tools._load_requirements(_PATH_ROOT),
+    install_requires=_load_requirements(),
 )
